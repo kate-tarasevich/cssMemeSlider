b42e18c458b52d41228c13367efb0912b6fb71b4
Katsiaryna Tarasevich <69838564+kate-tarasevich@users.noreply.github.com>
2024-09-15 19:48:57 +0200

    feat: edit js input to img and text Sun, Sep 15, 2024 07:47:08 PM

--- cssMemeSlider/script.js
+++ cssMemeSlider/script.js
@@ -1,12 +1,30 @@
-const contentItems = document.querySelectorAll(".item");
+const slides = [
+    {
+        image: "img/1.jpg",
+        text: "When your landlord says no dogs allowed"
+    },
+    {
+        image: "img/2.jpg",
+        text: "When mom called you a good boy"
+    },
+    {
+        image: "img/3.jpg",
+        text: "Mom: Smile nice for picture day<br>Dog: Okay, mom"
+    },
+    {
+        image: "img/4.jpg",
+        text: "Yes, I've been having fun, why do you ask?"
+    }
+];
+const sliderImage = document.getElementById("slider-image");
+const sliderText = document.getElementById("slider-text");
 const controlButtons = document.querySelectorAll(".btn");
 
 let currentIndex = 0;
 
 function showItem(index) {
-    contentItems.forEach((item, i) => {
-        item.classList.toggle("item-active", i === index);
-    });
+    sliderImage.src = slides[index].image;
+    sliderText.innerHTML = slides[index].text;
 
     controlButtons.forEach((btn, i) => {
         btn.classList.toggle("active", i === index);
@@ -20,3 +38,5 @@ controlButtons.forEach((btn, index) => {
         showItem(currentIndex);
     });
 });
+
+showItem(currentIndex);
