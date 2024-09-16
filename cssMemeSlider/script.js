4f6a45a46d176b5ea119230ad5ad981fe64bc88c
Katsiaryna Tarasevich <69838564+kate-tarasevich@users.noreply.github.com>
2024-09-16 18:41:43 +0200

    refactor: delete alert (Mon, Sep 16, 2024 06:41:26 PM)

--- cssMemeSlider/script.js
+++
@@ -1,3 +0,0 @@
-if (confirm("Сори, не заметила, что надо ссылку на pull request оставлять, она будет по ссылке")) {
-    window.location.href = "https://github.com/kate-tarasevich/cssMemeSlider/pull/1";
-}
